ae315dde8dce6dfc4fe738c0c5904c295a1ef6ee
bahromzokirov <bahromzokirov820@gmail.com>
2021-12-12 14:55:42 +0500

    Наша первые изменения

--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -4,22 +4,36 @@ import (
 	"bank/pkg/bank/types"
 	"fmt"
 )
-func ExampleTotal() {
-cards := []types.Card{
-	{
-		Balance: 10_000_00,
-		Active: true,
-	},
-	{
-		Balance: 10_000_00,
-		Active: true,
-	},
-	{
-		Balance: 10_000_00,
-		Active: true,
-	},
-}	
-	fmt.Println(Total(cards))
-	//Output: 2000000
+
+func ExamplePaymentSources() {
+	cards := []types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  true,
+			PAN:  "5058 1241 5784 8888",
+		},
+		{
+			Balance: 10_000_00,
+			Active:  false,
+			PAN:  "5058 1242 5784 8888",
+		},
+		{
+			Balance: -10_000_00,
+			Active:  true,
+			PAN:  "5058 1243 5784 8888",
+		},
+		{
+			Balance: 2_000_00,
+			Active:  true,
+			PAN:  "5058 1244 5784 8888",
+		},
+	}
+		sources := PaymentSources(cards)
+		for _, source := range sources {
+			fmt.Println(source.Number)
+		}
 	
+	//Output:
+	// 5058 1241 5784 8888
+	// 5058 1244 5784 8888
 }
